4f4e28518bce09624862ea2622dccbecbec6649a
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2022-03-25 13:53:50 +0100

    add jupyter plugins

--- packer/plugins.lua
+++ packer/plugins.lua
@@ -53,6 +53,8 @@ return require('packer').startup(function(use)
   use 'mracos/mermaid.vim'
   use 'ojroques/vim-oscyank'
   use 'danymat/neogen'
+  use 'untitled-ai/jupyter_ascending.vim'
+  use 'goerz/jupytext.vim'
 
   if packer_bootstrap then
     require('packer').sync()
