04ead3eba68e49b2585ec3aacbecf8546521f930
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2022-06-17 11:12:44 +0000

    add dap shortcuts

--- plug-config/_dap.lua
+++ plug-config/_dap.lua
@@ -12,3 +12,10 @@
      remoteRoot = "."
    }
  })
+
+ local keymap = vim.keymap.set
+ local dap = require"dap"
+ local dap_python = require"dap-python"
+ keymap("n", "<leader>db", dap.toggle_breakpoint, {silent = true})
+ keymap("n", "<leader>dr", dap.repl.toggle, {silent = true})
+ keymap("n", "<leader>dm", dap_python.test_method, {silent = true})
