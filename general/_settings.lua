110034add10c48f26d12f1e4882594fa53d6746d
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2022-09-19 16:20:00 +0200

    add yanking highlight

--- general/_settings.lua
+++ general/_settings.lua
@@ -32,8 +32,9 @@ o.background = "dark"
 
 o.list = true
 
-group = vim.api.nvim_create_augroup("YankGroup", {clear = true})
+local yank_group = vim.api.nvim_create_augroup("YankGroup", {clear = true})
 vim.api.nvim_create_autocmd({"TextYankPost"},
-  {group = group, callback = function()
+  {group = yank_group, callback = function()
     vim.highlight.on_yank({higroup="IncSearch", timeout=100}) 
   end})
+
