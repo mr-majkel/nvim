08d7fc48202fb1b5afea837eb98613d4147252f7
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2022-09-20 13:19:50 +0200

    ayu differentiate current

--- plug-config/_ayu.lua
+++ plug-config/_ayu.lua
@@ -2,6 +2,12 @@ local colors = require('ayu.colors')
 colors.generate()
 
 require('ayu').setup({
-    mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-    overrides = {String = {fg = colors.vcs_added}}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+  mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+  overrides = {
+    String = {fg = colors.vcs_added},
+    LineNr = {fg="#5eacd3"},
+    VertSplit = {fg = "#555555", bg="#111111"},
+    NormalNC = {bg = "#111111"}
+
+  }, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
 })
