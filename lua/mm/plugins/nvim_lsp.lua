1bdebe0b74f7100296c2fd5e39abe8c861651594
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2022-09-30 13:52:03 +0200

    update for 0.8

--- lua/mm/plugins/nvim_lsp.lua
+++ lua/mm/plugins/nvim_lsp.lua
@@ -47,9 +47,9 @@ local on_attach = function(client, bufnr)
 --  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.document_formatting then
     buf_set_keymap("n", "<leader>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
+  elseif client.server_capabilities.document_range_formatting then
     buf_set_keymap("n", "<leader>gf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
