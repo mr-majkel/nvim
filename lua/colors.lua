802fcbe03a529a3a61578c3c9af8dde26aa9ec59
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2022-10-20 15:31:24 +0200

    update material to new version

--- lua/colors.lua
+++ lua/colors.lua
@@ -2,69 +2,57 @@
 vim.g.material_style="darker"
 
 require('material').setup({
-
     contrast = {
+        terminal = true, -- Enable contrast for the built-in terminal
         sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
         floating_windows = true, -- Enable contrast for floating windows
         cursor_line = true, -- Enable darker background for the cursor line
         non_current_windows = true, -- Enable darker background for non-current windows
-        popup_menu = true, -- Enable lighter background for the popup menu
+        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
     },
-
-    italics = {
-        comments = true, -- Enable italic comments
-        keywords = false, -- Enable italic keywords
-        functions = false, -- Enable italic functions
-        strings = false, -- Enable italic strings
-        variables = false -- Enable italic variables
+    styles = { -- Give comments style such as bold, italic, underline etc.
+        comments = { italic = true },
+        strings = { --[[ bold = true ]] },
+        keywords = { --[[ underline = true ]] },
+        functions = { --[[ bold = true, undercurl = true ]] },
+        variables = {},
+        operators = {},
+        types = {},
     },
-
-    contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
-        "terminal", -- Darker terminal background
-        "packer", -- Darker packer background
-        "qf" -- Darker qf list background
+    plugins = { -- Uncomment the plugins that you use to highlight them
+        "dap",
+        -- "dashboard",
+        "gitsigns",
+        -- "hop",
+        -- "indent-blankline",
+        -- "lspsaga",
+        -- "mini",
+        -- "neogit",
+        "nvim-cmp",
+        -- "nvim-navic",
+        -- "nvim-tree",
+        -- "sneak",
+        "telescope",
+        "trouble",
+        "which-key",
     },
-
-    high_visibility = {
-        lighter = false, -- Enable higher contrast text for lighter style
-        darker = true -- Enable higher contrast text for darker style
-    },
-
     disable = {
         colored_cursor = false, -- Disable the colored cursor
         borders = false, -- Disable borders between verticaly split windows
         background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
-        term_colors = false, -- Prevent the theme from setting terminal colors
+        term_colors = true, -- Prevent the theme from setting terminal colors
         eob_lines = false -- Hide the end-of-buffer lines
     },
-
+    high_visibility = {
+        lighter = false, -- Enable higher contrast text for lighter style
+        darker = true, -- Enable higher contrast text for darker style
+    },
     lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
-
     async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+    custom_colors = nil, -- If you want to everride the default colors, set this to a function
+    custom_highlights = {}, -- Overwrite highlights with your own  Michal Modzelewski, 2022-10-12 - reworking lazy loading
+  })
 
-    custom_highlights = {}, -- Overwrite highlights with your own
-
-    plugins = { -- Here, you can disable(set to false) plugins that you don't use or don't want to apply the theme to
-        trouble = true,
-        nvim_cmp = true,
-        neogit = true,
-        gitsigns = true,
-        git_gutter = true,
-        telescope = true,
-        nvim_tree = true,
-        sidebar_nvim = true,
-        lsp_saga = true,
-        nvim_dap = true,
-        nvim_navic = true,
-        which_key = true,
-        sneak = true,
-        hop = true,
-        indent_blankline = true,
-        nvim_illuminate = true,
-        mini = true,
-    }
-})
--- AYU
 -- local colors = require('ayu.colors')
 -- colors.generate()
 
