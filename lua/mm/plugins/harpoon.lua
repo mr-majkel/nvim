3f33b7138ef68ffc44d35e8117c55c51c572a79d
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2022-11-30 10:02:41 +0100

    some qol improvements to harpoon and dap

--- lua/mm/plugins/harpoon.lua
+++ lua/mm/plugins/harpoon.lua
@@ -1,9 +1,9 @@
 local keymap = vim.keymap.set
 require("telescope").load_extension('harpoon')
 
-keymap("n", "<leader>gg", function() vim.api.nvim_command("Telescope harpoon marks") end)
-keymap("n", "<leader>aa", require("harpoon.mark").add_file)
-keymap("n", "<leader>ga", function() require("harpoon.ui").nav_file(1) end)
-keymap("n", "<leader>go", function() require("harpoon.ui").nav_file(2) end)
-keymap("n", "<leader>ge", function() require("harpoon.ui").nav_file(3) end)
-keymap("n", "<leader>gu", function() require("harpoon.ui").nav_file(4) end)
+keymap("n", "<leader>gg", function() vim.api.nvim_command("Telescope harpoon marks") end, {desc="Telescope harpoon marks"})
+keymap("n", "<leader>ga", require("harpoon.mark").add_file, {desc="Harpoon a file"})
+keymap("n", "<leader>go", function() require("harpoon.ui").nav_file(1) end, {desc="Go to harpoon 1"})
+keymap("n", "<leader>ge", function() require("harpoon.ui").nav_file(2) end, {desc="Go to harpoon 2"})
+keymap("n", "<leader>gu", function() require("harpoon.ui").nav_file(3) end, {desc="Go to harpoon 3"})
+keymap("n", "<leader>gh", function() require("harpoon.ui").nav_file(4) end, {desc="Go to harpoon 4"})
