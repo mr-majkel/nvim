a2dddf48150dbc32862e06c6e3527cb33361541e
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2023-01-26 15:31:28 +0100

    fix dap

--- lua/mm/plugins/dap.lua
+++ lua/mm/plugins/dap.lua
@@ -20,4 +20,4 @@
  keymap("n", "<leader>db", dap.toggle_breakpoint, {silent = true, desc = "Toggle dap breakpoint"})
  keymap("n", "<leader>dr", dap.repl.toggle, {silent = true, desc = "Toggle dap repl"})
  keymap("n", "<leader>dm", dap_python.test_method, {silent = true, desc = "Run dap for current test method"})
- keymap("n", "<leader>dc", dap_python.test_method, {silent = true, desc = "Run dap for current test class"})
+ keymap("n", "<leader>dc", dap_python.test_class, {silent = true, desc = "Run dap for current test class"})
