3085137ad00ea33a24840898bf1b69d5130d5193
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2023-08-10 16:53:27 +0200

    fugitive silent, indent no highlight

--- lua/mm/plugins/fugitive.lua
+++ lua/mm/plugins/fugitive.lua
@@ -1,9 +1,9 @@
 local map = vim.keymap.set
 
-map("n", "<leader>ee", ":Git<return>")
-map("n", "<leader>ef", ":Git fetch<return>")
-map("n", "<leader>ep", ":Git pull<return>")
-map("n", "<leader>eh", ":Git push<return>")
+map("n", "<leader>ee", ":Git<return>", {silent=true})
+map("n", "<leader>ef", ":Git fetch<return>", {silent=true})
+map("n", "<leader>ep", ":Git pull<return>", {silent=true})
+map("n", "<leader>eh", ":Git push<return>", {silent=true})
 map("n", "<leader>ec", ":Git checkout ")
 map("n", "<leader>em", ":Git commit --no-verify -m '")
 
