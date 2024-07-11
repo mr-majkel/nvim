eaafb0e1b10057b2de2d73816bf5eedb143f7e00
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2024-07-11 14:54:21 +0200

    remove neopyter - it was slowing down python files

--- init.lua
+++ init.lua
@@ -14,7 +14,13 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup('plugins')
+require("lazy").setup({
+  spec = "plugins",
+  rocks= {
+    enabled=true,
+    hererocks=true,
+  },
+})
 
 require 'mm.general.my_keys'
 
