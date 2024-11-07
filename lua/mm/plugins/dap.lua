1db365152958c2132eb23358bd046bf38f6e6bbe
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2024-11-07 14:51:17 +0100

    add conditional breakpoint

--- lua/mm/plugins/dap.lua
+++ lua/mm/plugins/dap.lua
@@ -18,6 +18,7 @@
  local dap = require"dap"
  local dap_python = require"dap-python"
  keymap("n", "<leader>db", dap.toggle_breakpoint, {silent = true, desc = "Toggle dap breakpoint"})
+ keymap("n", "<leader>dB", function() dap.toggle_breakpoint(vim.fn.input("Condition for breakpoint: ")) end, {silent = true, desc = "Toggle dap breakpoint condition"})
  keymap("n", "<leader>dr", dap.repl.toggle, {silent = true, desc = "Toggle dap repl"})
  keymap("n", "<leader>dm", dap_python.test_method, {silent = true, desc = "Run dap for current test method"})
  keymap("n", "<leader>dc", dap_python.test_class, {silent = true, desc = "Run dap for current test class"})
